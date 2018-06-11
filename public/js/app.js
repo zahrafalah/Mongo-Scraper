24fd7626f227a8701dfab57584483ca84912c0d4
zahra falah <sepidehfallah2@gmail.com>
2018-06-11 16:57:48 -0700

    routes/handlebars/db

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,82 @@
 //     })
 // });
 
-// //Set clicked nav option to active
-// $(".navbar-nav li").click(function() {
-//     $(".navbar-nav li").removeClass("active");
-//     $(this).addClass("active");
-// });
+//Set clicked nav option to active
+$(".navbar-nav li").click(function() {
+    $(".navbar-nav li").removeClass("active");
+    $(this).addClass("active");
+});
+
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+});
+
+
+
+
+
+//Handle Save Article button
+$(".save").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function(data) {
+        window.location = "/"
+    })
+});
+
+//Handle Delete Article button
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/delete/" + thisId
+    }).done(function(data) {
+        window.location = "/saved"
+    })
+});
+
+//Handle Save Note button
+$(".saveNote").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    if (!$("#noteText" + thisId).val()) {
+        alert("please enter a note to save")
+    }else {
+      $.ajax({
+            method: "POST",
+            url: "/notes/save/" + thisId,
+            data: {
+              text: $("#noteText" + thisId).val()
+            }
+          }).done(function(data) {
+              // Log the response
+              console.log(data);
+              // Empty the notes section
+              $("#noteText" + thisId).val("");
+              $(".modalNote").modal("hide");
+              window.location = "/saved"
+          });
+    }
+});
+
+//Handle Delete Note button
+$(".deleteNote").on("click", function() {
+    var noteId = $(this).attr("data-note-id");
+    var articleId = $(this).attr("data-article-id");
+    $.ajax({
+        method: "DELETE",
+        url: "/notes/delete/" + noteId + "/" + articleId
+    }).done(function(data) {
+        console.log(data)
+        $(".modalNote").modal("hide");
+        window.location = "/saved"
+    })
+});
+
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var path = require("path");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
@@ -10,11 +11,30 @@ var cheerio = require("cheerio");
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+//setting up Morgan middleware
+// app.use(logger('dev'));
+
 var PORT = 3000;
 var app = express();
+
 var exphbs = require("express-handlebars");
 
-app.use(logger("dev"));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+  partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
+
+// Set Handlebars as the view engine
+// app.engine('handlebars', exphbs({ 
+//   defaultLayout: 'main',
+//   partialsDir: __dirname + "/views/partials"
+//    }));
+//   app.set('view engine', 'handlebars');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
@@ -25,7 +45,7 @@ mongoose.connect("mongodb://localhost/mongo-scraper");
 
 // Routes
 
-//GET requests to render Handlebars pages
+///GET requests to render Handlebars pages
 app.get("/", function(req, res) {
   Article.find({"saved": false}, function(error, data) {
     var hbsObject = {
@@ -44,6 +64,7 @@ app.get("/saved", function(req, res) {
     res.render("saved", hbsObject);
   });
 });
+
 // A GET request to scrape the wjs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
@@ -77,6 +98,138 @@ app.get("/scrape", function(req, res) {
 
   });
 
+});
+
+
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      var hbsObject = {
+        article: dbArticle
+      }
+      // console.log(JSON.stringify(hbsObject));
+      res.render("home", hbsObject);
+    })
+  
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Save an article
+app.post("/articles/save/:id", function(req, res) {
+  // Use the article id to find and update its saved boolean
+  Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true})
+  // Execute the above query
+  .exec(function(err, dbArticle) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+    }
+    else {
+      // Or send the document to the browser
+      res.send(dbArticle);
+    }
+  });
+});
+
+// Delete an article
+app.post("/articles/delete/:id", function(req, res) {
+  // Use the article id to find and update its saved boolean
+  Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": false, "notes": []})
+  // Execute the above query
+  .exec(function(err, dbArticle) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+    }
+    else {
+      // Or send the document to the browser
+      res.send(dbArticle);
+    }
+  });
+});
+
+// Create a new note
+app.post("/notes/save/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note({
+    body: req.body.text,
+    article: req.params.id
+  });
+  console.log(req.body)
+  // And save the new note the db
+  newNote.save(function(error, note) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's notes
+      Article.findOneAndUpdate({ "_id": req.params.id }, {$push: { "notes": note } })
+      // Execute the above query
+      .exec(function(err) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        else {
+          // Or send the note to the browser
+          res.send(note);
+        }
+      });
+    }
+  });
+});
+
+// Delete a note
+app.delete("/notes/delete/:note_id/:article_id", function(req, res) {
+  // Use the note id to find and delete it
+  Note.findOneAndRemove({ "_id": req.params.note_id }, function(err) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.article_id }, {$pull: {"notes": req.params.note_id}})
+       // Execute the above query
+        .exec(function(err) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+            res.send(err);
+          }
+          else {
+            // Or send the note to the browser
+            res.send("Note Deleted");
+          }
+        });
+    }
+  });
 });
 
 // Start the server
